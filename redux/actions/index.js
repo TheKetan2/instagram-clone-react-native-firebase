c098cda8842013a15fe28eab6425a797f142879e
Ketan <ketantoo@gmail.com>
2021-02-12 12:19:50 +0530

    refeed screen is working

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,11 +1,21 @@
-import firebase from "firebase";
 import {
   USER_STATE_CHANGE,
-  USER_POST_STATE_CHANGE,
+  USER_POSTS_STATE_CHANGE,
   USER_FOLLOWING_STATE_CHANGE,
   USERS_DATA_STATE_CHANGE,
+  USERS_POSTS_STATE_CHANGE,
+  USERS_LIKES_STATE_CHANGE,
+  CLEAR_DATA,
 } from "../constants/index";
+import firebase from "firebase";
+import { SnapshotViewIOSComponent } from "react-native";
+require("firebase/firestore");
 
+export function clearData() {
+  return (dispatch) => {
+    dispatch({ type: CLEAR_DATA });
+  };
+}
 export function fetchUser() {
   return (dispatch) => {
     firebase
@@ -15,11 +25,7 @@ export function fetchUser() {
       .get()
       .then((snapshot) => {
         if (snapshot.exists) {
-          console.log(snapshot.data());
-          dispatch({
-            type: USER_STATE_CHANGE,
-            currentUser: snapshot.data(),
-          });
+          dispatch({ type: USER_STATE_CHANGE, currentUser: snapshot.data() });
         } else {
           console.log("does not exist");
         }
@@ -42,8 +48,7 @@ export function fetchUserPosts() {
           const id = doc.id;
           return { id, ...data };
         });
-        console.log(posts);
-        dispatch({ type: USER_POST_STATE_CHANGE, posts: posts });
+        dispatch({ type: USER_POSTS_STATE_CHANGE, posts });
       });
   };
 }
@@ -60,16 +65,17 @@ export function fetchUserFollowing() {
           const id = doc.id;
           return id;
         });
-        console.log(following);
         dispatch({ type: USER_FOLLOWING_STATE_CHANGE, following });
+        for (let i = 0; i < following.length; i++) {
+          dispatch(fetchUsersData(following[i], true));
+        }
       });
   };
 }
 
-export function fetchUsersData(uid) {
+export function fetchUsersData(uid, getPosts) {
   return (dispatch, getState) => {
     const found = getState().usersState.users.some((el) => el.uid === uid);
-
     if (!found) {
       firebase
         .firestore()
@@ -78,19 +84,17 @@ export function fetchUsersData(uid) {
         .get()
         .then((snapshot) => {
           if (snapshot.exists) {
-            console.log(snapshot.data());
             let user = snapshot.data();
             user.uid = snapshot.id;
-            dispatch({
-              type: USERS_DATA_STATE_CHANGE,
-              user,
-            });
 
-            dispatch(fetchUsersFollowingPosts(user.id));
+            dispatch({ type: USERS_DATA_STATE_CHANGE, user });
           } else {
             console.log("does not exist");
           }
         });
+      if (getPosts) {
+        dispatch(fetchUsersFollowingPosts(uid));
+      }
     }
   };
 }
@@ -106,9 +110,6 @@ export function fetchUsersFollowingPosts(uid) {
       .get()
       .then((snapshot) => {
         const uid = snapshot.query.EP.path.segments[1];
-
-        console.log(snapshot, uid);
-
         const user = getState().usersState.users.find((el) => el.uid === uid);
 
         let posts = snapshot.docs.map((doc) => {
@@ -116,12 +117,34 @@ export function fetchUsersFollowingPosts(uid) {
           const id = doc.id;
           return { id, ...data, user };
         });
-        console.log(posts);
-        dispatch({ type: USERS_POST_STATE_CHANGE, posts: posts });
 
-        for (let i = 0; i < following.length; i++) {
-          dispatch(fetchUsersData(following[i]));
+        for (let i = 0; i < posts.length; i++) {
+          dispatch(fetchUsersFollowingLikes(uid, posts[i].id));
         }
+        dispatch({ type: USERS_POSTS_STATE_CHANGE, posts, uid });
+      });
+  };
+}
+
+export function fetchUsersFollowingLikes(uid, postId) {
+  return (dispatch, getState) => {
+    firebase
+      .firestore()
+      .collection("posts")
+      .doc(uid)
+      .collection("userPosts")
+      .doc(postId)
+      .collection("likes")
+      .doc(firebase.auth().currentUser.uid)
+      .onSnapshot((snapshot) => {
+        const postId = snapshot.ZE.path.segments[3];
+
+        let currentUserLike = false;
+        if (snapshot.exists) {
+          currentUserLike = true;
+        }
+
+        dispatch({ type: USERS_LIKES_STATE_CHANGE, postId, currentUserLike });
       });
   };
 }
